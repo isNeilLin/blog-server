3dfe2a9759b1e2845e4b4123fb3bf150d3a951e3
Lin Pengfei <1264766856@qq.com>
2018-02-07 15:21:38 +0800

    authorizator

--- router/router.go
+++ router/router.go
@@ -3,31 +3,63 @@ package router
 import (
 	"blog/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/appleboy/gin-jwt"
+	"time"
 )
 
 func InitRouter() {
 	// 初始化
-	r := gin.Default()
-
+	r 		:= gin.Default()
+	auth 	:= r.Group("auth")
+	r.POST("/login", authMiddleWare.LoginHandler)
 	// 获取文章列表
-	r.GET("/get_all", controllers.GetAll)
 	r.GET("/get_publish", controllers.GetPublish)
-	r.GET("/get_private", controllers.GetPrivate)
-
-	// 操作文章
-	r.GET("/post", controllers.GetPostById)
-	r.POST("/post", controllers.Post)
-	r.POST("/update", controllers.Update)
-	r.POST("/delete", controllers.Delete)
-
 	// 标签相关
 	r.GET("/tags",controllers.GetTags)
-	r.POST("/create_tag",controllers.AddTag)
-	r.POST("/update_tag",controllers.UpdateTag)
-	r.POST("/delete_tag",controllers.DeleteTag)
-
 	// 归档
 	r.GET("/archive",controllers.GetArchive)
 
+	auth.Use(authMiddleWare.MiddlewareFunc())
+	{
+		auth.GET("/get_all", controllers.GetAll)
+		auth.GET("/get_private", controllers.GetPrivate)
+		// 操作文章
+		auth.GET("/post", controllers.GetPostById)
+		auth.POST("/post", controllers.Post)
+		auth.POST("/update", controllers.Update)
+		auth.POST("/delete", controllers.Delete)
+		auth.POST("/create_tag",controllers.AddTag)
+		auth.POST("/update_tag",controllers.UpdateTag)
+		auth.POST("/delete_tag",controllers.DeleteTag)
+	}
+
 	r.Run()
+}
+
+var authMiddleWare = &jwt.GinJWTMiddleware{
+	Realm: 			"test zone",
+	Key:			[]byte("the little prince"),
+	Timeout:		time.Hour,
+	MaxRefresh:		time.Hour,
+	Authenticator:  func(user string, password string, c *gin.Context) (string, bool) {
+		if user == "admin" && password == "admin" {
+			return user, true
+		}
+		return user, false
+	},
+	Authorizator:   func(user string, c *gin.Context) bool {
+		if user == "admin" {
+			return true
+		}
+		return false
+	},
+	Unauthorized:	func(c *gin.Context, code int, message string) {
+		c.JSON(code, gin.H{
+			"code": 	code,
+			"message":	message,
+		})
+	},
+	TokenLookup:	"header:Authorizator",
+	TokenHeadName:	"Bearer",
+	TimeFunc:		time.Now,
 }
